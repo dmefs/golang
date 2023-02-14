d4c713dd510c1b4b069659c2f85a70455bf9fbad
dmefs <dennis.mefs@gmail.com>
2023-02-14 14:21:58 +0800

    ch3: Add comma

---
+++ ch3/comma/comma.go
@@ -0,0 +1,16 @@
+// comma inserts commas in a non-negative decimal integer string.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(comma("12345"))
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
