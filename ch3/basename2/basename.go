b16d9349aa718b11dc1bc16e210ae834881e924a
dmefs <dennis.mefs@gmail.com>
2023-02-14 14:21:47 +0800

    ch3: Add basename2

---
+++ ch3/basename2/basename.go
@@ -0,0 +1,23 @@
+// basename removes directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(basename("a/b/c.go"))
+	fmt.Println(basename("c.d.go"))
+	fmt.Println(basename("abc"))
+}
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
